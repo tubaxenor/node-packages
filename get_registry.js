cb39aa9cc9d9ead4e0fc90f61a70d325b7c4e3b5
xenor <xenor@codegreenit.com>
2013-02-18 18:02:46 +0800

    add more meat on tweet

--- get_registry.js
+++ get_registry.js
@@ -1,15 +1,15 @@
 var request = require('request'),
-		config = require('./config'),
-		util = require('util'),
-		twitter = require('twitter'),
-		fs = require('fs'),
-		winston = require('winston');
+    config = require('./config'),
+    util = require('util'),
+    twitter = require('twitter'),
+    fs = require('fs'),
+    winston = require('winston');
 
 var twit_cli = new twitter(config.twitter);
 fs.exists('logs', function(res){
-	if(!res) {
-		fs.mkdir("logs");
-	}
+  if(!res) {
+    fs.mkdir("logs");
+  }
 });
 
 var logLevel = {
@@ -39,32 +39,32 @@ var logger = new (winston.Logger)({
 logger.exitOnError = false;
 logger.setLevels(logLevel.levels);
 function getNpmRegistry() {
-	time = Date.now()-1000*60*5; //check update in 15 mins
-	request("https://registry.npmjs.org/-/all/since?stale=update_after&startkey="+time, function(e, res, body){
-		if (!e && res.statusCode == 200) {
-			data = JSON.parse(body)
-			for(key in data) {
-				if(key != '_updated'){
-					logger.info("package_name : "+key)
-					msg = "https://npmjs.org/package/"+key+" "+data[key].description;
-					if(msg.length > 140) {
-						msg = msg.substring(0,137)+"...";
-					}
-					twit_cli
-					  .verifyCredentials(function(data){
-					  	//logger.info(util.inspect(data));
-					  })
-					  .updateStatus(msg, function(data) {
-				      //logger.info(util.inspect(data));
-				    });
-				}
-			}
-			//console.log(util.inspect(data))
-		} else {
-			logger.error("errors : "+e+" response : "+res)
-		}
-		setTimeout(getNpmRegistry, (1000*60*5))
-	})
+  time = Date.now()-1000*60*5; //check update in 15 mins
+  request("https://registry.npmjs.org/-/all/since?stale=update_after&startkey="+time, function(e, res, body){
+    if (!e && res.statusCode == 200) {
+      data = JSON.parse(body)
+      for(key in data) {
+        if(key != '_updated'){
+          logger.info("package_name : "+key)
+          msg = key + " " +data[key]["dist-tags"].latest + " https://npmjs.org/package/"+key+" "+data[key].description;
+          if(msg.length > 140) {
+            msg = msg.substring(0,137)+"...";
+          }
+          twit_cli
+            .verifyCredentials(function(data){
+              //logger.info(util.inspect(data));
+            })
+            .updateStatus(msg, function(data) {
+              //logger.info(util.inspect(data));
+            });
+        }
+      }
+      //console.log(util.inspect(data))
+    } else {
+      logger.error("errors : "+e+" response : "+res)
+    }
+    setTimeout(getNpmRegistry, (1000*60*5))
+  })
 }
 
 getNpmRegistry();
